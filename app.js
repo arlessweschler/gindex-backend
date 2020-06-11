8e80a5f3c30e2c792f4657d8d5ab271fdb7159fb
Shan.tk <tksudharshan@gmail.com>
2020-06-11 18:34:36 +0530

    Added CORS

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+}
+
+app.use(allowCrossDomain)
+
 mongoose.connect(process.env.DBURL, {useNewUrlParser: true,  useUnifiedTopology: true})
 
 const site = process.env.SITE;
